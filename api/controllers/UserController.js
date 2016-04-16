31cfc9b7fa00ef264e2761d79e09eda67c7d330a
Vinod Beloshe <vinodbeloshe12@gmail.com>
2016-04-16 18:34:24 +0530

    first git

---
+++ api/controllers/UserController.js
@@ -0,0 +1,111 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+  save: function(req, res) {
+    if (req.body) {
+      User.saveData(req.body, function(err, data) {
+        if (err) {
+          res.json({
+            value: false,
+            data: err
+          });
+        } else {
+          res.json({
+            value: true,
+            data: data
+          });
+        }
+      });
+    } else {
+      res.json({
+        value: false,
+        data: "Invalid Call"
+      });
+    }
+  },
+  delete: function(req, res) {
+    if (req.body) {
+      if (req.body._id && req.body._id != "") {
+        User.deleteData(req.body, function(err, data) {
+          if (err) {
+            res.json({
+              value: false,
+              data: err
+            });
+          } else {
+            res.json({
+              value: true,
+              data: data
+            });
+          }
+        });
+      } else {
+        res.json({
+          value: false,
+          data: "Invalid Id"
+        });
+      }
+    } else {
+      res.json({
+        value: false,
+        data: "Invalid Call"
+      });
+    }
+  },
+  getAll: function(req, res) {
+    if (req.body) {
+      User.getAll(req.body, function(err, data) {
+        if (err) {
+          res.json({
+            value: false,
+            data: err
+          });
+        } else {
+          res.json({
+            value: true,
+            data: data
+          });
+        }
+      });
+    } else {
+      res.json({
+        value: false,
+        data: "Invalid Call"
+      });
+    }
+  },
+  getOne: function(req, res) {
+    if (req.body) {
+      if (req.body._id && req.body._id != "") {
+        User.getOne(req.body, function(err, data) {
+          if (err) {
+            res.json({
+              value: false,
+              data: err
+            });
+          } else {
+            res.json({
+              value: true,
+              data: data
+            });
+          }
+        });
+      } else {
+        res.json({
+          value: false,
+          data: "Invalid Id"
+        });
+      }
+    } else {
+      res.json({
+        value: false,
+        data: "Invalid Call"
+      });
+    }
+  }
+};
---
+++ api/services/User.js
@@ -0,0 +1,74 @@
+/**
+ * User.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var schema = new Schema({
+  name: String,
+  email: String
+});
+module.exports = mongoose.model("User", schema);
+var model = {
+  saveData: function(data, callback) {
+    var user = this(data);
+    if (data._id) {
+      this.findOneAndUpdate({
+        _id:data._id
+      },data,function(err,data2){
+        if (err) {
+          console.log(err);
+          callback(err, null);
+        } else {
+          callback(null, data2);
+        }
+      });
+    } else {
+      user.save(function(err, data2) {
+        if (err) {
+          console.log(err);
+          callback(err, null);
+        } else {
+          callback(null, data2);
+        }
+      });
+    }
+  },
+  deleteData:function(data,callback){
+    this.findOneAndRemove({
+      _id:data._id
+    },data,function(err,data2){
+      if (err) {
+        console.log(err);
+        callback(err, null);
+      } else {
+        callback(null, data2);
+      }
+    });
+  },
+  getAll:function(data,callback){
+    this.find(function(err,data2){
+      if (err) {
+        console.log(err);
+        callback(err, null);
+      } else {
+        callback(null, data2);
+      }
+    });
+  },
+  getOne:function(data,callback){
+    this.findOne({
+      _id:data._id
+    },function(err,data2){
+      if (err) {
+        console.log(err);
+        callback(err, null);
+      } else {
+        callback(null, data2);
+      }
+    });
+  },
+};
+module.exports = _.assign(module.exports, model);
